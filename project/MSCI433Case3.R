9412c97d78db5f4dcf95b34c4561d566453de0c0
MayyarAA <mayyar1235@gmail.com>
2022-04-08 18:29:49 -0400

    linear programming

--- project/MSCI433Case3.R
+++ project/MSCI433Case3.R
@@ -19,8 +19,8 @@ matricQ1 = matrix(c(
                     #<.50
                     .5,-.5,-.5,-.5,
                     -.5,.5,-.5,-.5,
-                    -.5,-.5,1.5,-.5,
-                    -.5,-.5,-.5,1.5,
+                    -.5,-.5,.5,-.5,
+                    -.5,-.5,-.5,.5,
                     #<=10mill yr1
                     (.2*27000),(.2*22000),(.2*18000),(.2*14000)
                     
@@ -32,3 +32,55 @@ constraintSingsQ1=c(">=",">=",">=",">=",    "<=","<=","<=","<="
 rhsQ1 = c(0,0,0,0  ,0,0,0,0   ,10000000   )
 optimalsolnQ1 = lp("max",objectiveFcnQ1,matricQ1,constraintSingsQ1,rhsQ1,compute.sens = TRUE)
 print(optimalsolnQ1)
+
+print(optimalsolnQ1$status)
+
+optSolnValues <-optimalsolnQ1$solution
+names(optSolnValues) <- c("x1","x2","x3","x4")
+print(optSolnValues)
+
+
+
+#Q5
+
+
+
+objectiveFcnQ1 = c(9719.848771,3355.009452,7488.090737,4815.879017)
+
+
+matricQ1 = matrix(c(
+  #<.15
+  .85,-.15,-.15,-.15,
+  -.15,.85,-.15,-.15,
+  -.15,-.15,.85,-.15,
+  -.15,-.15,-.15,.85,
+  #<.50
+  .5,-.5,-.5,-.5,
+  -.5,.5,-.5,-.5,
+  -.5,-.5,.5,-.5,
+  -.5,-.5,-.5,.5,
+  #<=10mill yr1
+  (.2*27000),(.2*22000),(.2*18000),(.2*14000)
+  
+), nrow =9,byrow= TRUE)
+
+constraintSingsQ1=c(">=",">=",">=",">=",    "<=","<=","<=","<="    
+                    ,"<=")
+#0.15
+rhsQ1 = c(0,0,0,0  ,0,0,0,0   ,10000000   )
+optimalsolnQ1 = lp("max",objectiveFcnQ1,matricQ1,constraintSingsQ1,rhsQ1,compute.sens = TRUE)
+print(optimalsolnQ1)
+
+optSolnValues <-optimalsolnQ1$solution
+names(optSolnValues) <- c("x1","x2","x3","x4")
+print(optSolnValues)
+
+
+
+
+
+
+
+
+
+#Q7 make contraint just 25%
