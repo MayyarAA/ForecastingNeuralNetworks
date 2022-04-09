221d5569080d2ec5ce5ef6fd754b0e80423e40c3
MayyarAA <mayyar1235@gmail.com>
2022-04-09 12:02:10 -0400

    done

--- project/MSCI433Case3.R
+++ project/MSCI433Case3.R
@@ -83,4 +83,68 @@ print(optSolnValues)
 
 
 
-#Q7 make contraint just 25%
+#Q6 
+
+
+objectiveFcnQ1 = c(10545.45455,3603.305785,8132.231405,5223.140496)
+
+
+matricQ1 = matrix(c(
+  #<.15
+  .85,-.15,-.15,-.15,
+  -.15,.85,-.15,-.15,
+  -.15,-.15,.85,-.15,
+  -.15,-.15,-.15,.85,
+  #<.50
+  .5,-.5,-.5,-.5,
+  -.5,.5,-.5,-.5,
+  -.5,-.5,.5,-.5,
+  -.5,-.5,-.5,.5,
+  #<=10mill yr1
+  (.2*27000),(.2*22000),(.2*18000),(.2*14000)
+  
+), nrow =9,byrow= TRUE)
+
+constraintSingsQ1=c(">=",">=",">=",">=",    "<=","<=","<=","<="    
+                    ,"<=")
+#0.15
+rhsQ1 = c(0,0,0,0  ,0,0,0,0   ,10000000   )
+optimalsolnQ1 = lp("max",objectiveFcnQ1,matricQ1,constraintSingsQ1,rhsQ1,compute.sens = TRUE)
+print(optimalsolnQ1)
+
+optSolnValues <-optimalsolnQ1$solution
+names(optSolnValues) <- c("x1","x2","x3","x4")
+print(optSolnValues)
+
+
+
+#Q7  make contraint just 25%
+
+
+objectiveFcnQ1 = c(11125.61983,4076.033058,8519.008264,5523.966942)
+
+
+matricQ1 = matrix(c(
+  #<.15
+  .75,-.25,-.25,-.25,
+  -.25,.75,-.25,-.25,
+  -.25,-.25,.75,-.25,
+  -.25,-.25,-.25,.75,
+  #<=10mill yr1
+  (.2*27000),(.2*22000),(.2*18000),(.2*14000)
+  
+), nrow =5,byrow= TRUE)
+
+constraintSingsQ1=c("==","==","==","==" 
+                    ,"<=")
+#0.15
+rhsQ1 = c(0,0,0,0 ,10000000   )
+optimalsolnQ1 = lp("max",objectiveFcnQ1,matricQ1,constraintSingsQ1,rhsQ1,compute.sens = TRUE)
+print(optimalsolnQ1)
+
+print(optimalsolnQ1$status)
+
+optSolnValues <-optimalsolnQ1$solution
+names(optSolnValues) <- c("x1","x2","x3","x4")
+print(optSolnValues)
+
